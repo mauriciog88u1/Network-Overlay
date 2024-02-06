4ed75d3b87518b701e75645f294282069e15b42f
mauriciog88u1 <83040257+mauriciog88u1@users.noreply.github.com>
2024-02-05 20:44:08 -0700

    graddle

--- src/main/java/csx55/overlay/node/MessagingNode.java
+++ src/main/java/csx55/overlay/node/MessagingNode.java
@@ -15,46 +15,46 @@ public class MessagingNode {
     private Socket registrySocket;
     private ServerSocket serverSocket;
 
-    public MessagingNode(String registryHost, int registryPort) {
-        debug_print("MessagingNode created with registry at: " + registryHost + " " + registryPort);
-        try {
-            this.registrySocket = new Socket(registryHost, registryPort);
-            this.sender = new TCPSender(registrySocket);
-            debug_print("Connected to registry at: " + registryHost + ":" + registryPort);
-
-            this.serverSocket = new ServerSocket(0);
-            debug_print("MessagingNode listening on port: " + serverSocket.getLocalPort());
-        } catch (Exception e) {
-            debug_print("Error initializing MessagingNode: " + e.getMessage());
-        }
-    }
-
-
-    public static void main(String[] args) {
-        System.out.println("MessagingNode starting");
-        if (args.length != 2 && args.length != 3) {
-            System.out.println("Usage: java MessagingNode <registry host> <registry port> --DEBUG");
-            return;
-        }
-        if (args.length == 3 && (args[2].equals("--DEBUG"))) {
-            DEBUG = true;
-            debug_print("DEBUG mode enabled");
-        }
-        String registryHost = args[0];
-        int registryPort = Integer.parseInt(args[1]);
-        MessagingNode messagingNode = new MessagingNode(registryHost, registryPort);
-        messagingNode.registerWithRegistry();
-    }
-
-    public void registerWithRegistry() {
-
-        try {
-            InetAddress localHost = InetAddress.getLocalHost();
-            Register register = new Register(localHost, serverSocket.getLocalPort());
-            sender.sendMessage(register.getBytes());
-            debug_print("Registration message sent: " + localHost.getHostAddress() + " " + serverSocket.getLocalPort());
-        } catch (Exception e) {
-            debug_print("Error registering with registry: " + e.getMessage());
-        }
-    }
+//    public MessagingNode(String registryHost, int registryPort) {
+//        debug_print("MessagingNode created with registry at: " + registryHost + " " + registryPort);
+//        try {
+//            this.registrySocket = new Socket(registryHost, registryPort);
+//            this.sender = new TCPSender(registrySocket);
+//            debug_print("Connected to registry at: " + registryHost + ":" + registryPort);
+//
+//            this.serverSocket = new ServerSocket(0);
+//            debug_print("MessagingNode listening on port: " + serverSocket.getLocalPort());
+//        } catch (Exception e) {
+//            debug_print("Error initializing MessagingNode: " + e.getMessage());
+//        }
+//    }
+//
+//
+//    public static void main(String[] args) {
+//        System.out.println("MessagingNode starting");
+//        if (args.length != 2 && args.length != 3) {
+//            System.out.println("Usage: java MessagingNode <registry host> <registry port> --DEBUG");
+//            return;
+//        }
+//        if (args.length == 3 && (args[2].equals("--DEBUG"))) {
+//            DEBUG = true;
+//            debug_print("DEBUG mode enabled");
+//        }
+//        String registryHost = args[0];
+//        int registryPort = Integer.parseInt(args[1]);
+//        MessagingNode messagingNode = new MessagingNode(registryHost, registryPort);
+//        messagingNode.registerWithRegistry();
+//    }
+//
+//    public void registerWithRegistry() {
+//
+//        try {
+//            InetAddress localHost = InetAddress.getLocalHost();
+//            Register register = new Register(localHost, serverSocket.getLocalPort());
+//            sender.sendMessage(register.getBytes());
+//            debug_print("Registration message sent: " + localHost.getHostAddress() + " " + serverSocket.getLocalPort());
+//        } catch (Exception e) {
+//            debug_print("Error registering with registry: " + e.getMessage());
+//        }
+//    }
 }
