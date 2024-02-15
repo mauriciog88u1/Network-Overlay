0d24be3b241db3e944dc7d2ef03d23500095240c
Mauricio <83040257+mauriciog88u1@users.noreply.github.com>
2024-02-14 18:17:14 -0700

    Update testScript.sh

--- testScript.sh
+++ testScript.sh
@@ -1,25 +1,17 @@
 #!/bin/bash
 
-# Navigate to the project directory
 cd /s/chopin/n/under/maur88/cs455/hw1/HW1_ATTEMPT_2
 
-# Rebuild the project using Gradle
 ./gradlew build
 
-# Start a new TMUX session
 tmux new-session -d -s "ssh_sessions"
 
-# Define the command to be executed on each machine
 command="/usr/bin/env /usr/lib/jvm/java-11-openjdk-11.0.21.0.9-2.el8.x86_64/bin/java -cp /s/chopin/n/under/maur88/cs455/hw1/HW1_ATTEMPT_2/build/resources/main:/s/chopin/n/under/maur88/cs455/hw1/HW1_ATTEMPT_2/build/classes/java/main csx55.overlay.node.MessagingNode sardine 1024 "
 
-# Define the list of machines
 machines=("bogota" "breckenridge" "buttermilk" "cooper" "copper-mtn" "crested-butte" "eldora" "keystone" "monarch" "steamboat")
 
-# Loop through each machine and execute the command via SSH in a separate TMUX window
 for machine in "${machines[@]}"; do
     tmux new-window -t "ssh_sessions" -n "$machine" "ssh $machine \"$command\"; read -p 'Press enter to close this session...'"
 done
 
-# Attach to the TMUX session to monitor the processes
 tmux attach-session -t "ssh_sessions"
-
